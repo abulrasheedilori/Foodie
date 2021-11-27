71a7c5dc4365e0455cd4f6023907d8ce314c71af
AbdulRasheed Ilori <54009597+hayyohmyde@users.noreply.github.com>
2021-11-27 19:20:26 +0100

    Recipe layout and other layout created

--- app/src/main/java/com/brainstem/foodie/MainActivity.kt
+++ app/src/main/java/com/brainstem/foodie/MainActivity.kt
@@ -43,12 +43,6 @@ class MainActivity : AppCompatActivity() {
         //appBarConfiguration = AppBarConfiguration(navController.graph)
         //setupActionBarWithNavController(navController, appBarConfiguration)
 
-
-
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -68,8 +62,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
+        return navController.navigateUp(appBarConfiguration)|| super.onSupportNavigateUp()
     }
 
 }
--- app/src/main/java/com/brainstem/foodie/RecipeFragment.kt
+++ app/src/main/java/com/brainstem/foodie/RecipeFragment.kt
@@ -7,31 +7,29 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.brainstem.foodie.databinding.FragmentRecipeBinding
+import kotlinx.android.synthetic.main.fragment_recipe.view.*
 
 class RecipeFragment : Fragment() {
 
     private var _binding: FragmentRecipeBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-
         _binding = FragmentRecipeBinding.inflate(inflater, container, false)
+        binding.recyclerView.showShimmer()
         return binding.root
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-//        binding.buttonFirst.setOnClickListener {
-//            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-//        }
+        binding.floatingActionButton.setOnClickListener {
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        }
     }
 
     override fun onDestroyView() {
